73511e2057465cb3b99779967b2fec10822f1069
AZmarill <yuuki01123583@gmail.com>
2025-05-16 15:59:44 +0900

    dirの分割

--- scripts/transfer_rsync.sh
+++ scripts/transfer_rsync.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-LOCAL_DIR="/mnt/c/wisteria_data/"
-mkdir -p $LOCAL_DIR
+LOCAL_DIR_1="/mnt/c/wisteria_data/push/"
+LOCAL_DIR_2="/mnt/c/wisteria_data/pull/"
+mkdir -p $LOCAL_DIR_1
+mkdir -p $LOCAL_DIR_2
 
 if [-z "$1"] || [-z "$2"]; then
    echo "スクリプトの後にユーザー名とモードを入力してください: $0 <ユーザー名> [push|pull]"
@@ -15,11 +17,11 @@ MODE="$2"
 
 if ["$MODE"="push"]; then
    echo "ユーザ ${REMOTE_USER}として${REMOTE_HOST}にデータを転送します~"
-   rsync -avzh --progress "$LOCAL_DIR" "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}"
+   rsync -avzh --progress "$LOCAL_DIR_1" "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}"
 
 elif ["$MODE"="pull"]; then
    echo "ユーザ ${REMOTE_USER}として${REMOTE_HOST}からデータを取得します~"
-   rsync -avzh --progress "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}" "$LOCAL_DIR"
+   rsync -avzh --progress "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}" "$LOCAL_DIR_2"
 
 else
    echo"モードが設定されていません。pushもしくはpullを指定してください。"
