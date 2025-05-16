07ae9d9c3036be67be908d370d1224bd002109ed
AZmarill <yuuki01123583@gmail.com>
2025-05-16 15:52:22 +0900

    双方向受信機能を追加

--- scripts/transfer_rsync.sh
+++ scripts/transfer_rsync.sh
@@ -3,14 +3,25 @@
 LOCAL_DIR="/mnt/c/wisteria_data/"
 mkdir -p $LOCAL_DIR
 
-if [-z "$1"]; then
-   echo "スクリプトの後にユーザー名を入力してください: $0 <ユーザー名>"
+if [-z "$1"] || [-z "$2"]; then
+   echo "スクリプトの後にユーザー名とモードを入力してください: $0 <ユーザー名> [push|pull]"
    exit 1
 fi
 
 REMOTE_USER="$1"
 REMOTE_HOST="wisteria.t.u-tokyo.ac.jp"
 REMOTE_DIR="/home/${REMOTTE_USER}/data/"
+MODE="$2"
 
-echo "ユーザ ${REMOTE_USER}として${REMOTE_HOST}にデータを転送します~"
-rsync -avzh --progress "$LOCAL_DIR" "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}"
+if ["$MODE"="push"]; then
+   echo "ユーザ ${REMOTE_USER}として${REMOTE_HOST}にデータを転送します~"
+   rsync -avzh --progress "$LOCAL_DIR" "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}"
+
+elif ["$MODE"="pull"]; then
+   echo "ユーザ ${REMOTE_USER}として${REMOTE_HOST}からデータを取得します~"
+   rsync -avzh --progress "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}" "$LOCAL_DIR"
+
+else
+   echo"モードが設定されていません。pushもしくはpullを指定してください。"
+   exit 1
+fi
